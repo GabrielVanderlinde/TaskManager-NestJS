8d1e1d96609709009e8b129b2e04a6cf42d2869c
Gabriel Vanderlinde <gabrielvanderlindezz@gmail.com>
2026-02-03 10:37:41 -0300

    docs: cria estrutura de documentacao atraves do swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { AppController2 } from './app2.controller'
 
 @Module({
   imports: [],
-  controllers: [AppController, AppController2],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
